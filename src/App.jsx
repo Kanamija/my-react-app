1276cb051c08768d6b95b55a86402bc62c1cf701
Kanami Anderson <kanamianderson@gmail.com>
2025-11-26 14:35:35 -0600

    The unit I am covereing talked about Prop types and default props. I think it is a bit outdated since we will be using typescript now. I figured out how to desctructure the prop object and set the default values in the Student parameters instead.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
     <>
       <Student name='Sponge Bob' age={30} isStudent={true} />
       <Student name='Patrick' age={5} isStudent={true} />
+      <Student name='Sandy' age={72} isStudent={false} />
+      <Student name='Squidward' age={27} isStudent={false} />
+      <Student />
     </>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,11 +1,14 @@
-function Student(props) {
+
+
+function Student({name = "Guest", age = 0, isStudent = false}) {
     return (
         <div className='student'>
-            <p>Name: {props.name}</p>
-            <p>Age: {props.age}</p>
-            <p>Student: {props.isStudent ? "Yes" : "No" }</p>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Student: {isStudent ? "Yes" : "No" }</p>
         </div>
     )
 }
+
 
 export default Student
