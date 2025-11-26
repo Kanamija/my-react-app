5059e12b37e52c8f8fdceecc7e0d7c5620c538e6
Kanami Anderson <kanamianderson@gmail.com>
2025-11-26 12:46:06 -0600

    Starting new module on props. We have created a Student compenent and are learning how to send data via parent child connection.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Button from "./Button.jsx"
+import Student from './Student.jsx'
 
 function App() {
-  return(<Button />)
+  return(
+    <>
+      <Student name='Sponge Bob' age={30} isStudent={true} />
+      <Student name='Patrick' age={5} isStudent={true} />
+    </>
+  )
 }
 
 export default App
