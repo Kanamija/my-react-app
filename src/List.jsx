6ba395082c678aef50b91c85a81b5015c0c5073f
Kanami Anderson <kanamianderson@gmail.com>
2025-12-30 16:57:14 -0600

    Finished the section on creating lists on React.

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,24 @@
-function List() {
-    const fruits = [
-        {id: 1, name: 'apple', calories: 95}, 
-        {id: 2, name: 'orange', calories: 45}, 
-        {id: 3, name: 'banana', calories: 105}, 
-        {id: 4, name: 'coconut', calories: 159}, 
-        {id: 5, name: 'pineapple', calories: 37}
-    ];
+import PropTypes from 'prop-types';
 
+function List(props) {
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b> </li>);
-    return(<ol>{listItems}</ol>);
+    const category = props.category;
+    const itemList =props.items;
+
+    const listItems = itemList.map(item => <li key={item.id}>{item.name}: &nbsp; <b>{item.calories}</b> </li>);
+    return( <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+            </>);
 }
-
+List.PropTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number, 
+                                               name: PropTypes.string,
+                                               calories: PropTypes.number })),
+}
+List.defaultProps = {
+    category: "Category",
+    items: [],
+}
 export default List
